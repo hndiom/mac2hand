58e11a899bb57be79b1f0a81a3383afac279402e
unayung <Unayung@gmail.com>
2013-01-21 00:05:48 +0800

    SEO

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
 SeoHelper.configure do |config|
   config.skip_blank               = false
-  config.site_name = Setting.app_name
-  config.default_page_description = "PAGE_DESSCRIPTION"
-  config.default_page_keywords    = "PAGE_KEYWORDS"
-  config.default_page_image = Setting.domain + Setting.default_logo_url
-  config.site_name_formatter  = lambda { |title, site_name|   "#{title} « #{site_name}".html_safe }
-  config.pagination_formatter = lambda { |title, page_number| "#{title} - Page No.#{page_number}" }
+  config.site_name                = Setting.app_name
+  config.default_page_description = Setting.description
+  config.default_page_keywords    = ""
+  config.default_page_image       = ""
+  config.site_name_formatter      = lambda { |title, site_name|   "#{title} « #{site_name}".html_safe }
+  config.pagination_formatter     = lambda { |title, page_number| "#{title} - Page No.#{page_number}" }
 
 end
